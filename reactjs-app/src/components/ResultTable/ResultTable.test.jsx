67ef892384c2efb8cfd79c06403adb3e5b57fad4
Pablo Vaz <vazsk8@gmail.com>
2023-09-13 11:12:13 -0300

    tests adjusts

--- reactjs-app/src/components/ResultTable/ResultTable.test.jsx
+++ reactjs-app/src/components/ResultTable/ResultTable.test.jsx
@@ -1,10 +1,10 @@
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import { Loader } from '../'
+import { ResultTable } from '../'
 
 describe('Reders tests', () => {
-	test('Render Loader component', () => {
-		const { container } = render(<Loader />);
+	test('Render ResultTable component', () => {
+		const { container } = render(<ResultTable items={[]}/>);
 		expect(container).toMatchSnapshot();
 	});
 })
